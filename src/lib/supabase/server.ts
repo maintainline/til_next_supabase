46eb491e33c3bc150c1b3973e1373b417af6adeb
maintainline <z.seon.dev@gmail.com>
2025-10-27 11:08:58 +0900

    [docs] 테이블 셋팅 및 타입 추출

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,14 +1,29 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { Database } from '@/types/database.types';
+import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
-  return createServerClient(supabaseUrl!, supabaseKey!, {
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const createClient = async () => {
+  // 최신 문법 처리
+  const cookieStore = await cookies();
+
+  return createServerClient<Database>(supabaseUrl!, supabaseKey!, {
     cookies: {
       getAll() {
         return cookieStore.getAll();
       },
       setAll(cookiesToSet) {
-        /* 쿠키 설정 로직 */
+        try {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          );
+        } catch {
+          // The `setAll` method was called from a Server Component.
+          // This can be ignored if you have middleware refreshing
+          // user sessions.
+        }
       },
     },
   });
