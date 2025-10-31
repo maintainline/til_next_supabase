e9cf5665a2097633d98850a7c94068621d6503a9
maintainline <z.seon.dev@gmail.com>
2025-10-31 12:44:57 +0900

    [docs] password 진행중..

--- src/app/(default)/layout.tsx
+++ src/app/(default)/layout.tsx
@@ -11,7 +11,7 @@ export default async function ProtectedLayout({
   // 세션 정보가 있는지 없는지 기다립니다..
   const { data } = await supabase.auth.getSession();
   // 세션 정보를 가져왔는데 null 아니라면 비회원이다.
-  if (!data.session) redirect('/');
+  if (data.session) redirect('/');
 
   return <>{children}</>;
 }
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -11,6 +11,7 @@ export default async function ProtectedLayout({
   // 세션 정보가 있는지 없는지 기다립니다..
   const { data } = await supabase.auth.getSession();
   // 세션 정보를 가져왔는데 null 이라면 비회원이다.
+  console.log(data);
   if (!data.session) redirect('/signin');
 
   return <>{children}</>;
