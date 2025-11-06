83386eb336886316e28f47332ca1e50e7493c6ba
maintainline <z.seon.dev@gmail.com>
2025-11-06 11:55:11 +0900

    [docs] 데이터 무효화 및 낙관적 업데이트

--- src/hooks/todos/queries/useTodoDataById.ts
+++ src/hooks/todos/queries/useTodoDataById.ts
@@ -1,9 +1,10 @@
 import { fetchTodoById } from '@/apis/todo';
+import { QUERY_KEYS } from '@/lib/constants';
 import { useQuery } from '@tanstack/react-query';
 
 export function useTodoDataById(id: number) {
   return useQuery({
-    queryKey: ['todos', id],
+    queryKey: QUERY_KEYS.todo.detail(id.toString()),
     queryFn: () => fetchTodoById(id),
     // 5초동안 fresh 유효기간
     staleTime: 5000,
