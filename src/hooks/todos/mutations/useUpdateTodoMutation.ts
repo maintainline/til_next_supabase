37f68a2d43ea921ed81a1eeec101f9f775029a69
maintainline <z.seon.dev@gmail.com>
2025-11-07 10:34:25 +0900

    [docs] 데이터 무효화 및 낙관적 업데이트

--- src/hooks/todos/mutations/useUpdateTodoMutation.ts
+++ src/hooks/todos/mutations/useUpdateTodoMutation.ts
@@ -1,23 +1,51 @@
 import { updateTodo } from '@/apis/todo';
 import { Todo } from '@/apis/todo-type';
 import { QUERY_KEYS } from '@/lib/constants';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  QueryClient,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
 
 export function useUpdateTodoMutation() {
-  // 1.전역 접근용 변수
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: updateTodo,
-    // mutate 함수가 실행 되는 시점에 작성
-    onMutate: updatedTodo => {
-      // 캐시 데이터를 업데이트 기능 작성
-      // 2. 데이터 일부만 수정함.
+    onMutate: async updatedTodo => {
+      // 요청취소 기능 구현
+      await queryClient.cancelQueries({ queryKey: QUERY_KEYS.todo.list });
+
+      // 원본데이트를 보관함.
+      const originTodos = queryClient.getQueryData<Todo[]>(
+        QUERY_KEYS.todo.list
+      );
+
+      // 낙관적 업데이트 진행함.
       queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, prevTodos => {
         if (!prevTodos) return [];
         return prevTodos.map(todo =>
           todo.id === updatedTodo.id ? { ...todo, ...updatedTodo } : todo
         );
       });
+
+      // 원상복구할 데이터를 리턴해 준다.
+      return { originTodos };
+    },
+    // 에러가 발생함
+    onError: (error, valiable, context) => {
+      console.log(error);
+      if (context?.originTodos) {
+        queryClient.setQueryData<Todo[]>(
+          QUERY_KEYS.todo.list,
+          context.originTodos
+        );
+      }
+      return { error };
+    },
+    // 요청이 완료됨
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: QUERY_KEYS.todo.list });
     },
   });
 }
+
