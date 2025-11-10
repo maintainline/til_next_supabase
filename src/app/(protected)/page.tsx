6df4c4b29de31e1802828bcde25e5637b5580701
maintainline <z.seon.dev@gmail.com>
2025-11-10 12:50:57 +0900

    [docs] 포스트 등록 기본 구성

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,14 +1,10 @@
+import { CreatePostButton } from '@/components/post/CreatePostButton';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
-      <Button>버튼</Button>
-      <Button variant={'destructive'}>버튼</Button>
-      <Button variant={'secondary'}>버튼</Button>
-      <Button variant={'outline'}>버튼</Button>
-      <Button variant={'ghost'}>버튼</Button>
-      <Button variant={'link'}>버튼</Button>
+    <div className='flex flex-col gap-10'>
+      <CreatePostButton />
     </div>
   );
 }
