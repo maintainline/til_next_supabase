ddd19f82815194eabd510de3110d1044391dcd50
maintainline <z.seon.dev@gmail.com>
2025-11-24 13:02:03 +0900

    [docs] 회원 탈퇴

--- src/components/profile/ProfileInfo.tsx
+++ src/components/profile/ProfileInfo.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image';
 import { useEffect } from 'react';
 import EditProfileButton from './EditProfileButton';
 import { useSession } from '@/stores/session';
+import DeleteProfileButton from './DeleteProfileButton';
 
 export default function ProfileInfo({ userId }: { userId: string }) {
   // 세션 정보 참조하기(zustand 보관됨)
@@ -41,8 +42,14 @@ export default function ProfileInfo({ userId }: { userId: string }) {
         <div className=' text-muted-foreground'>{profile?.bio}</div>
         <div className='text-muted-foreground'>{profile?.role}</div>
       </div>
+
       {/* 프로필 수정 */}
-      {isMine && <EditProfileButton />}
+      {isMine && (
+        <div>
+          <EditProfileButton />
+          <DeleteProfileButton />
+        </div>
+      )}
     </div>
   );
 }
